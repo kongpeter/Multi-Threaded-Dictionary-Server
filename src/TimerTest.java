85e7d8842e4f3e63a893a6654f896d706a461860
kongpeter <xjtlu2014@outlook.com>
2019-09-05 15:35:48 +1000

    Initial commit

---
+++ src/TimerTest.java
@@ -0,0 +1,26 @@
+/*
+ * Name: Lingxuan Kong
+ * Email: lingxuan.kong14@gmail.com
+ * Student ID: 957828
+ * Github: kongpeter
+ * Homepage: kongpeter.github.io
+ * Copyright (c) 2019.
+ */
+
+import java.util.*;
+public class TimerTest
+{
+    public static void main(String[] args)
+    {
+        Timer timer = new Timer();// 实例化Timer类
+        timer.schedule(new TimerTask()
+        {
+            public void run()
+            {
+                System.out.println("退出");
+                this.cancel();
+            }
+        }, 5000);// 这里百毫秒
+        System.out.println("本程序存在5秒后自动退出");
+    }
+}
